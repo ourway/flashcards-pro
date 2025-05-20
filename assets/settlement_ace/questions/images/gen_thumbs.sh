d55b498cea0ff96eef87b6c753bc0ffdfcf230e6
Farshid Ashouri <farsheed.ashouri@gmail.com>
2025-05-20 20:42:15 +0100

    added 128X128 thumbs as well.

--- assets/settlement_ace/questions/images/gen_thumbs.sh
+++ assets/settlement_ace/questions/images/gen_thumbs.sh
@@ -10,16 +10,30 @@ find . -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) -print0 | whi
   filename=$(basename "$file")
   # Get the filename without the extension
   name_no_ext="${filename%.*}"
-  # Define the output filename
-  output_filename="./thumbnails/${name_no_ext}_48x48.jpg"
 
-  # Check if the thumbnail already exists
-  if [ ! -f "$output_filename" ]; then
+  # Define the output filename for 48x48 thumbnail
+  output_filename_48="./thumbnails/${name_no_ext}_48x48.jpg"
+  # Define the output filename for 128x128 thumbnail
+  output_filename_128="./thumbnails/${name_no_ext}_128x128.jpg"
+
+  # Process 48x48 thumbnail
+  # Check if the 48x48 thumbnail already exists
+  if [ ! -f "$output_filename_48" ]; then
     # Resize the image and save it to the thumbnails directory
-    magick "$file" -resize 48x48! "$output_filename"
-    echo "Resized $filename to $output_filename"
+    magick "$file" -resize 48x48! "$output_filename_48"
+    echo "Resized $filename to $output_filename_48"
   else
-    echo "Thumbnail $output_filename already exists. Skipping."
+    echo "Thumbnail $output_filename_48 already exists. Skipping."
+  fi
+
+  # Process 128x128 thumbnail
+  # Check if the 128x128 thumbnail already exists
+  if [ ! -f "$output_filename_128" ]; then
+    # Resize the image and save it to the thumbnails directory
+    magick "$file" -resize 128x128! "$output_filename_128"
+    echo "Resized $filename to $output_filename_128"
+  else
+    echo "Thumbnail $output_filename_128 already exists. Skipping."
   fi
 done
 
